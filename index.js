3762fb7ce422fa8b86c30445675166dbcc122ab9
OmkarJavanjal <a96738@gmail.com>
2019-11-14 13:17:45 +0530

    1 file modified

--- index.js
+++ index.js
@@ -930,9 +930,28 @@ carName = "Volvo";    // You can NOT use carName here
 const carName = "Volvo";*/
 
 
+//Async/Await
 
+//CallBack
+/**What is a Callback?
+Simply put: A callback is a function that is to be executed after another function has finished executing — hence the name ‘call back’.
+More complexly put: In JavaScript, functions are objects. Because of this, functions can take functions as arguments, and can be returned by other functions. Functions that do this are called higher-order functions. Any function that is passed as an argument is called a callback function. */
 
+function add(a, b , callback){ 
+   console.log(`The sum of ${a} and ${b} is ${a+b}.`); 
+   callback(); 
+   } 
+     
+   // disp() function is called just 
+   // after the ending of add() function  
+   function disp(){ 
+   console.log('This must be printed after addition'); 
+   } 
+     
+   // Calling add() function 
+   add(5,6,disp); 
 
+   
 
 
 
