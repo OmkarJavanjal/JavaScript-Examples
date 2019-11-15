0010f91acf2d98fd6439249202c2b6bac413fef8
OmkarJavanjal <a96738@gmail.com>
2019-11-15 16:15:38 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1071,9 +1071,86 @@ promise5
 
 
 
+//Async/Await
+/*What is Async/Await?
+The newest way to write asynchronous code in JavaScript.
+It is non blocking (just like promises and callbacks).
+Async/Await was created to simplify the process of working with and writing chained promises.
+Async functions return a Promise. If the function throws an error, the Promise will be rejected. If the function returns a value, the Promise will be resolved.
+
+Syntax:
+// Normal Function
+function add(x,y){
+  return x + y;
+}
+// Async Function
+async function add(x,y){
+  return x + y;
+}
+
+Await
+Async functions can make use of the await expression. This will pause the async function and wait for the Promise to resolve prior to moving on.
+
+ */
 
 
 
+
+
+
+
+
+
+
+
+//Without Async Await
+function doubleAfter2Seconds(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x * 2);
+    }, 2000);
+  });
+}
+
+function addPromise(x){
+  return new Promise(resolve => {
+    doubleAfter2Seconds(10).then((a) => {
+      doubleAfter2Seconds(20).then((b) => {
+        doubleAfter2Seconds(30).then((c) => {
+          resolve(x + a + b + c);
+      	})
+      })
+    })
+  });
+}
+
+//With Async Await
+function doubleAfter2Seconds(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x * 2);
+    }, 2000);
+  });
+}
+
+async function addAsync(x) {
+  const a = await doubleAfter2Seconds(10);
+  const b = await doubleAfter2Seconds(20);
+  const c = await doubleAfter2Seconds(30);
+  return x + a + b + c;
+}
+
+
+addAsync(10).then((sum) => {
+  console.log(sum);
+});
+
+addPromise(10).then((sum) => {
+  console.log('Sum without asynck await:', sum);
+});
+
+
+//
 
 
 
