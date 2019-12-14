6c6b9f93971ab7f139cbcfd578aea35bc205e090
OmkarJavanjal <a96738@gmail.com>
2019-12-14 13:30:41 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1264,4 +1264,32 @@ import {displayName} from './displayName';
 })()
 
 
+//Inheritance new obj example2
+
+function Person(firstName, lastName) {
+		this.FirstName = firstName || "unknown";
+		this.LastName = lastName || "unknown";            
+	}
+
+	Person.prototype.getFullName = function () {
+		return this.FirstName + " " + this.LastName;
+	}
+	function Student(firstName, lastName, schoolName, grade)
+	{
+		Person.call(this, firstName, lastName);
+
+		this.SchoolName = schoolName || "unknown";
+		this.Grade = grade || 0;
+	}
+	//Student.prototype = Person.prototype;
+	Student.prototype = new Person();
+	Student.prototype.constructor = Student;
+
+	var std = new Student("James","Bond", "XYZ", 10);
+
+  console.log('getfullname: ', std.getFullName());  //prints fullname 
+
+
+//example link: //https://www.tutorialsteacher.com/javascript/inheritance-in-javascript
+
 //For any issue you can reach me at github
